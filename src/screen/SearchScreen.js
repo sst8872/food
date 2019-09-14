8fb44ca44e7af9cb95b158732bc9d8d4dac4e019
unknown <sst8872>
2019-09-14 23:59:20 +0900

    error handling added

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../apis/yelp";
 
 const SearchScreen = () => {
-    const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: term,
-                location: 'san jose'
-            }
-        });
-        setResults(response.data.businesses);
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: term,
+          location: "san jose"
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong. Try again");
     }
+  };
 
-    return (
-        <View>
-            <SearchBar
-                term={term}
-                onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={searchApi}
-            />
-            <Text>Search Screen</Text>
-            <Text>We have found {results.length} results</Text>
-        </View>
-    );
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={searchApi}
+      />
+      {errorMessage ? (
+        <Text>{errorMessage}</Text>
+      ) : (
+        <Text>We have found {results.length} results</Text>
+      )}
+    </View>
+  );
 };
-
 
 const styles = StyleSheet.create({});
 
